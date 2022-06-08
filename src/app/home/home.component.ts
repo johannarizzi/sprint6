e886695340ab92bb81c7e35022c644cdc83ecf0b
Johanna Rizzi <rizzijohanna@gmail.com>
2022-06-08 12:59:09 +0200

    level 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface ShowSentence {  
+  id: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,9 +13,30 @@ import { Component, OnInit } from '@angular/core';
 
 export class HomeComponent implements OnInit {
 
+  homeSentence = [
+    {
+      "id": 1,
+      "text": "Nuestro héroe estaba flotando por el espacio sideral cuando a lo lejos divisó una nave espacial",
+    },
+    {
+      "id": 2,
+      "text": "Sentía curiosidad por el interior de la nave y se puso a inspeccionarla. Llegó a una sala con dos puertas",
+    },
+    {
+      "id": 3,
+      "text": "El héroe decidió atravesar la puerta que le llevaba a casa",
+    },
+    {
+      "id": 4,
+      "text": "Mientras tanto, otras heroes no tuvieron tanta suerte en su elección...",
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
+
